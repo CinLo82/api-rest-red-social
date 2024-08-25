e4859a701bf6499e4e867ecc4ae6ada3eee8d4bb
Cintia Losada <cinlos3382@gmail.com>
2024-08-25 00:11:02 -0300

    modelo de publicaciones- crear una publicación

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,3 +1,5 @@
+const Publication = require('../models/publication');
+
 const pruebaPublication = (req, res) => {
     return res.status(200).send(
         {
@@ -6,6 +8,48 @@ const pruebaPublication = (req, res) => {
     )
 }
 
+//guardar publicacion
+const savePublication = async (req, res) => {
+    //recoger los datos dle body
+    const params = req.body
+    if (!params.text) 
+        return res.status(200).send({ 
+            status: 'error',
+            message: 'Debes enviar un texto'
+        })
+
+    try {
+            //crear objeto de publicacion
+        let newPublication = new Publication()
+        newPublication.user = req.user.id
+        newPublication.text = params.text
+            //guardar publicacion en la base de datos
+        const publicationStored = await newPublication.save()
+        if (!publicationStored) 
+            return res.status(404).send({
+         message: 'La publicación no ha sido guardada' 
+        })
+        return res.status(200).send({ 
+            newPublication: publicationStored 
+        })
+    } catch (error) {
+        return res.status(500).send({ 
+            message: 'Error al guardar la publicación' 
+        })
+    }
+}
+
+//listar todas las publicaciones
+
+//listar public
+
+//borrar publicacion
+
+//subir archivos de imagen/avatar de usuario
+
+//obtener archivo de imagen/avatar de usuario
+
 module.exports = {
-    pruebaPublication
+    pruebaPublication,
+    savePublication
 }
--- routes/publication.js
+++ routes/publication.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const PublicationControler = require('../controllers/publication')
+const check = require('../middlewares/auth')
+
 
 // definir ruta
 router.get('/prueba-Publication', PublicationControler.pruebaPublication)
+router.post('/save', check.auth, PublicationControler.savePublication)
 
 //exportar router
 module.exports = router
